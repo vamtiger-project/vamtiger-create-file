86470ef8a4a27a9713bab23c133db1378566e177
James-Alex Matthews <vamtiger@vamtiger.com>
2017-12-12 22:59:57 +0200

    vamtiger-node-typescript-commit

--- source/index.ts
+++ source/index.ts
@@ -1,27 +1,4 @@
 import { writeFile } from 'fs';
-import * as Promise from 'bluebird';
-import { URL } from 'url';
+import { promisify } from 'util';
 
-export type test = typeof writeFile;
-
-export type VamtigerFile = string;
-
-export type VamtigerFileData = string;
-
-export type VamtigerFileOptions = string | IVamtigerOptions;
-
-export interface IVamtigerOptions {
-    encoding?: string | null;
-    mode?: number;
-    flag?: string;
-}
-
-export type VamtigerCreateFile = (path: VamtigerFile, data?: VamtigerFileData, options?: VamtigerFileOptions) => Promise<void>;
-
-const vamtigerCreateFile = Promise.promisify(writeFile,) as VamtigerCreateFile;
-
-export default (path: VamtigerFile, data?: VamtigerFileData, options?: VamtigerFileOptions) => {
-    const fileData = data ? data : '';
-
-    return vamtigerCreateFile(path, fileData, options);
-};
+export default promisify(writeFile);
