65901c61f852c55e4655b0147a2dc9f21a7c1570
James-Alex Matthews <vamtiger@vamtiger.com>
2017-12-12 23:00:02 +0200

    vamtiger-node-typescript-commit

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,16 +1,4 @@
 /// <reference types="node" />
-/// <reference types="bluebird" />
 import { writeFile } from 'fs';
-import * as Promise from 'bluebird';
-export declare type test = typeof writeFile;
-export declare type VamtigerFile = string;
-export declare type VamtigerFileData = string;
-export declare type VamtigerFileOptions = string | IVamtigerOptions;
-export interface IVamtigerOptions {
-    encoding?: string | null;
-    mode?: number;
-    flag?: string;
-}
-export declare type VamtigerCreateFile = (path: VamtigerFile, data?: VamtigerFileData, options?: VamtigerFileOptions) => Promise<void>;
-declare const _default: (path: string, data?: string | undefined, options?: string | IVamtigerOptions | undefined) => Promise<void>;
+declare const _default: typeof writeFile.__promisify__;
 export default _default;
--- build/index.js
+++ build/index.js
@@ -1,10 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = require("fs");
-const Promise = require("bluebird");
-const vamtigerCreateFile = Promise.promisify(fs_1.writeFile);
-exports.default = (path, data, options) => {
-    const fileData = data ? data : '';
-    return vamtigerCreateFile(path, fileData, options);
-};
+const util_1 = require("util");
+exports.default = util_1.promisify(fs_1.writeFile);
 //# sourceMappingURL=index.js.map
