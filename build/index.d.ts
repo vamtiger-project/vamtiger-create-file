5810061030c622d38e696978922637b37bc6a777
James-Alex Matthews <vamtiger@vamtiger.com>
2018-04-26 19:08:55 +0200

    vamtiger-node-typescript-commit

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,4 +1,4 @@
 /// <reference types="node" />
 import { writeFile } from 'fs';
-declare const _default: typeof writeFile.__promisify__;
-export default _default;
+declare const createFile: typeof writeFile.__promisify__;
+export default createFile;
--- build/index.js
+++ build/index.js
@@ -2,5 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = require("fs");
 const util_1 = require("util");
-exports.default = util_1.promisify(fs_1.writeFile);
+const bluebird = require("bluebird");
+const createFile = util_1.promisify && util_1.promisify(fs_1.writeFile) || bluebird.promisify(fs_1.writeFile);
+exports.default = createFile;
 //# sourceMappingURL=index.js.map
